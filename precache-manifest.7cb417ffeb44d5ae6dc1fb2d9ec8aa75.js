3a515d369efd504d900024488f5b78561dbfc261
PrateekVerma20 <prateekverma1998@gmail.com>
2020-04-27 01:08:50 +0530

    Updates

---
+++ precache-manifest.7cb417ffeb44d5ae6dc1fb2d9ec8aa75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78fb6291f479903c109b0377b656a6aa",
+    "url": "/Tic-Tac-Toe-Game/index.html"
+  },
+  {
+    "revision": "b52ff8c1e6de90ca0cf4",
+    "url": "/Tic-Tac-Toe-Game/static/css/main.2d3560bc.chunk.css"
+  },
+  {
+    "revision": "fec0e654214cc9661fb9",
+    "url": "/Tic-Tac-Toe-Game/static/js/2.59f395ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-Game/static/js/2.59f395ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52ff8c1e6de90ca0cf4",
+    "url": "/Tic-Tac-Toe-Game/static/js/main.d927089b.chunk.js"
+  },
+  {
+    "revision": "53e4c19be4808c8d7848",
+    "url": "/Tic-Tac-Toe-Game/static/js/runtime-main.4820a70e.js"
+  }
+]);
